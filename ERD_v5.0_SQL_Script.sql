a12b2e5ee1c27d90a305c66cc7f7e10cb5703150
Antonio Peavy <apeavy07@gmail.com>
2019-08-25 18:26:12 -0500

    adding files

---
+++ ERD_v5.0_SQL_Script.sql
@@ -0,0 +1,52 @@
+﻿-- v5.0
+-- This data model is not complete and was created to serve as a basis for getting started
+
+CREATE TABLE "Vehicles" (
+    "id" INT   NOT NULL,
+    "year" INT   NOT NULL,
+    "model" VARCHAR(255)   NOT NULL,
+    "vehicle_type" VARCHAR(255)   NOT NULL,
+    "brand" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Vehicles" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "Sources" (
+    "id" INT   NOT NULL,
+    -- Autotrader.com, Ebay.com, etc...
+    "name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_Sources" PRIMARY KEY (
+        "id"
+     )
+);
+
+-- This inventory table will be the result of joining the following entities together: "Vehicles", "Autotrader.com", "Ebay.com"
+CREATE TABLE "Inventory" (
+    "id" INT   NOT NULL,
+    "source_id" INT   NOT NULL,
+    "vehicle_id" INT   NOT NULL,
+    "condition" VARCHAR(255)   NOT NULL,
+    "exterior_color" VARCHAR(255)   NOT NULL,
+    "fuel_type" VARCHAR(255)   NOT NULL,
+    "interior_color" VARCHAR(255)   NOT NULL,
+    "make" VARCHAR(255)   NOT NULL,
+    "mileage" INT   NOT NULL,
+    "model" VARCHAR(255)   NOT NULL,
+    "price" MONEY   NOT NULL,
+    "transmission" VARCHAR(255)   NOT NULL,
+    "seller_name" VARCHAR(255)   NOT NULL,
+    "url" VARCHAR(MAX)   NOT NULL,
+    "vin" VARCHAR(100)   NOT NULL,
+    "year" INT   NOT NULL,
+    CONSTRAINT "pk_Inventory" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "Inventory" ADD CONSTRAINT "fk_Inventory_source_id" FOREIGN KEY("source_id")
+REFERENCES "Sources" ("id");
+
+ALTER TABLE "Inventory" ADD CONSTRAINT "fk_Inventory_vehicle_id" FOREIGN KEY("vehicle_id")
+REFERENCES "Vehicles" ("id");
+
